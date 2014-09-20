8d4c99fb60cc7f98d75c716474424e356082ebb5
Navjot <navjot.dznr@gmail.com>
2014-09-20 06:34:02 -0400

    static file service

--- server.js
+++ server.js
@@ -1,2 +1,48 @@
 
-var http = require('http');
+var http = require('http'),
+	url = require('url'),
+	fs = require('fs'),
+	cache = {};
+
+http.createServer(function(req, res) {
+	var filePath = false;
+
+	if (req.url === '/') {
+		filePath = 'public/index.html';
+	}
+
+	serveStatic(res, cache, './' + filePath);
+
+}).listen(8080, function() {console.log("server is now listening")});
+
+function sendFile(response, data) {
+	response.writeHead(200, {"content-type": "text/html"});
+	response.end(data);
+}
+
+function send404(response) {
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404: resource not found.');
+	response.end();
+}
+
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if (exists) {
+				fs.readFile(absPath, function(err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] =  data;
+						sendFile(response, data);
+					}
+				})
+			} else {
+				send404(response);
+			}
+		})
+	}
+}
