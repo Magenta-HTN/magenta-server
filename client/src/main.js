39cd8feca49da7644520d9bc7c00424c2a44d59e
Jathu Satkunarajah <jathu.satkunarajah@gmail.com>
2014-09-21 07:56:00 -0400

    Temp. files for test.

--- client/src/main.js
+++ client/src/main.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 					"value": "80%"
 				},{
 					"property": "height",
-					"value": "auto"
+					"value": "50px"
 				},{
 					"property": "padding",
 					"value": "3% 5%"
@@ -33,36 +33,66 @@ $(document).ready(function() {
 					"value": "1px solid red"
 				}
 			]
-		},{
-			"intent": "ADD",
-			"type": "h4",
-			"html": "Hello World",
-			"styles": [
-				{
-					"property": "color",
-					"value": "red"
-				},{
-					"property": "text-align",
-					"value": "center"
-				},{
-					"property": "font-style",
-					"value": "italic"
-				}
-			],
-			"parent": 1
-		},{
-			"intent": "ADD",
-			"type": "p",
-			"html": "lorem ipsum",
-			"styles": [
-				{
-					"property": "color",
-					"value": "#4F0025"
-				}
-			],
-			"parent": 1
 		}
 	];
+
+
+	// var initialInputs = [
+	// 	{
+	// 		"intent": "ADD",
+	// 		"type": "div",
+	// 		"html": "",
+	// 		"styles": [
+	// 			{
+	// 				"property": "background",
+	// 				"value": "white"
+	// 			},{
+	// 				"property": "width",
+	// 				"value": "80%"
+	// 			},{
+	// 				"property": "height",
+	// 				"value": "auto"
+	// 			},{
+	// 				"property": "padding",
+	// 				"value": "3% 5%"
+	// 			},{
+	// 				"property": "margin",
+	// 				"value": "5%"
+	// 			},{
+	// 				"property": "border",
+	// 				"value": "1px solid red"
+	// 			}
+	// 		]
+	// 	},{
+	// 		"intent": "ADD",
+	// 		"type": "h4",
+	// 		"html": "Hello World",
+	// 		"styles": [
+	// 			{
+	// 				"property": "color",
+	// 				"value": "red"
+	// 			},{
+	// 				"property": "text-align",
+	// 				"value": "center"
+	// 			},{
+	// 				"property": "font-style",
+	// 				"value": "italic"
+	// 			}
+	// 		],
+	// 		"parent": 1
+	// 	},{
+	// 		"intent": "ADD",
+	// 		"type": "p",
+	// 		"html": "lorem ipsum",
+	// 		"styles": [
+	// 			{
+	// 				"property": "color",
+	// 				"value": "#4F0025"
+	// 			}
+	// 		],
+	// 		"parent": 1
+	// 	}
+	// ];
 
 	// Add tests
 	_.forEach(initialInputs, function(inp) {
